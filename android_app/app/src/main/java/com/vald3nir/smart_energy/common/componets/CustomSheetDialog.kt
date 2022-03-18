9aa9fa1e9a90a14d4ff509ae8449df2696c323f8
Valdenir Severino <vald3nir@gmail.com>
2022-03-18 14:27:07 -0300

    show itens on menu

--- android_app/app/src/main/java/com/vald3nir/smart_energy/common/componets/CustomSheetDialog.kt
+++ android_app/app/src/main/java/com/vald3nir/smart_energy/common/componets/CustomSheetDialog.kt
@@ -2,13 +2,15 @@ package com.vald3nir.smart_energy.common.componets
 
 import android.content.Context
 import android.os.Bundle
-import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.vald3nir.smart_energy.databinding.CustomItemListBinding
 import com.vald3nir.smart_energy.databinding.CustomSheetDialogBinding
 
-class CustomSheetDialog(context: Context) : BottomSheetDialog(context) {
+class CustomSheetDialog(context: Context, private val items: List<CustomItemSheet>) :
+    BottomSheetDialog(context) {
 
     init {
         setCancelable(true)
@@ -16,32 +18,52 @@ class CustomSheetDialog(context: Context) : BottomSheetDialog(context) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val binding = CustomSheetDialogBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
-
+        binding.rvItems.apply {
+            adapter = SheetDialogAdapter()
+            layoutManager = LinearLayoutManager(context)
+        }
     }
 
-    inner class SheetDialogAdapter : RecyclerView.Adapter<ViewHolder>(){
+    inner class SheetDialogAdapter() :
+        RecyclerView.Adapter<ViewHolder>() {
 
+//        private val items = listOf(
+//            CustomItemSheet(title = "item", action = {}),
+//            CustomItemSheet(title = "item", action = {}),
+//            CustomItemSheet(title = "item", action = {}),
+//            CustomItemSheet(title = "item", action = {}),
+//            CustomItemSheet(title = "item", action = {}),
+//            CustomItemSheet(title = "item", action = {}),
+//        )
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            TODO("Not yet implemented")
+            return ViewHolder(CustomItemListBinding.inflate(layoutInflater))
         }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            TODO("Not yet implemented")
+            holder.bindView(items[position])
         }
 
-        override fun getItemCount(): Int {
-            TODO("Not yet implemented")
+        override fun getItemCount(): Int = items.size
+    }
+
+    inner class ViewHolder(itemView: CustomItemListBinding) :
+        RecyclerView.ViewHolder(itemView.root) {
+
+        fun bindView(item: CustomItemSheet) {
+            itemView.apply {
+                setOnClickListener { item.action.invoke() }
+            }
         }
     }
 
-    inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-    }
-
+    data class CustomItemSheet(
+        val title: String,
+        val action: () -> Unit
+    )
 
 }
--- android_app/app/src/main/java/com/vald3nir/smart_energy/presentation/dashboard/DashboardActivity.kt
+++ android_app/app/src/main/java/com/vald3nir/smart_energy/presentation/dashboard/DashboardActivity.kt
@@ -1,11 +1,10 @@
 package com.vald3nir.smart_energy.presentation.dashboard
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.vald3nir.smart_energy.R
+import com.vald3nir.smart_energy.common.componets.CustomSheetDialog
 import com.vald3nir.smart_energy.common.core.BaseActivity
 import com.vald3nir.smart_energy.common.extensions.appendNewData
 import com.vald3nir.smart_energy.common.extensions.loadData
@@ -38,7 +37,7 @@ class DashboardActivity : BaseActivity() {
                 btnBack.isVisible = false
                 title.setText(R.string.app_name)
                 btnConfig.isVisible = true
-                btnConfig.setOnClickListener{
+                btnConfig.setOnClickListener {
                     openMenu()
                 }
             }
@@ -71,31 +70,19 @@ class DashboardActivity : BaseActivity() {
         }
     }
 
-    private fun openMenu(){
-        val dialog = BottomSheetDialog(this)
-        val view = layoutInflater.inflate(R.layout.custom_sheet_dialog, null)
+    private fun openMenu() {
 
-        // on below line we are creating a variable for our button
-        // which we are using to dismiss our dialog.
-//        val btnClose = view.findViewById<Button>(R.id.idBtnDismiss)
-//
-//        // on below line we are adding on click listener
-//        // for our dismissing the dialog button.
-//        btnClose.setOnClickListener {
-//            // on below line we are calling a dismiss
-//            // method to close our dialog.
-//            dialog.dismiss()
-//        }
-        // below line is use to set cancelable to avoid
-        // closing of dialog box when clicking on the screen.
+        val items = listOf(
+            CustomSheetDialog.CustomItemSheet(title = "item", action = {}),
+            CustomSheetDialog.CustomItemSheet(title = "item", action = {}),
+            CustomSheetDialog.CustomItemSheet(title = "item", action = {}),
+            CustomSheetDialog.CustomItemSheet(title = "item", action = {}),
+            CustomSheetDialog.CustomItemSheet(title = "item", action = {}),
+            CustomSheetDialog.CustomItemSheet(title = "item", action = {}),
+        )
+
+        val dialog = CustomSheetDialog(this, items)
         dialog.setCancelable(true)
-
-        // on below line we are setting
-        // content view to our view.
-        dialog.setContentView(view)
-
-        // on below line we are calling
-        // a show method to display a dialog.
         dialog.show()
     }
 }
